2567d774dd3579f6eaf954075fa8a0f7246c3653
Robin <hello@robinblomberg.se>
2020-12-29 15:17:56 +0100

    Alter API and add createClasses function

--- lib/nodes.d.ts
+++ lib/nodes.d.ts
@@ -1,15 +1,22 @@
 import * as AST from './ast';
 
-export function declaration(
+export function createClasses(
+  ...payload: (AST.Rule[] | AST.StyleRulesObject)[]
+): {
+  classes: Record<string, string>;
+  styleSheet: AST.StyleSheet;
+};
+
+export function createStyleDeclaration(
   property: string,
   value: AST.StyleDeclarationValue
 ): AST.StyleDeclaration;
 
-export function styleRule(
+export function createStyleRule(
   selector: string,
   ...payload: AST.StyleDeclarationsPayload[]
 ): AST.StyleRule;
 
-export function styleSheet(
+export function createStyleSheet(
   ...payload: (AST.Rule[] | AST.StyleRulesObject)[]
 ): AST.StyleSheet;
